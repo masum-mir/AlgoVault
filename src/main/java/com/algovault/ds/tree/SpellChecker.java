dfe175fa8312c4836fb60633d3a0e4010d0b0948
masum <masummir773@gmail.com>
2025-08-10 02:02:37 +0600

    add btree

---
+++ src/main/java/com/algovault/ds/tree/SpellChecker.java
@@ -0,0 +1,9 @@
+package com.algovault.ds.tree;
+
+import java.util.TreeSet;
+
+public class SpellChecker {
+    public static void main(String[] args) {
+        AVLTree treeSet = new AVLTree();
+    }
+}
